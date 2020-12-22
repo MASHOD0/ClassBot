1086ab6b08ba727cd20f038b96a8a4d2398996c1
MASHOD0 <mashhood2002@gmail.com>
2020-12-22 16:11:11 +0530

    added comandline argument feature

--- googlemeet/main.py
+++ googlemeet/main.py
@@ -6,12 +6,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 def main():
-    time.sleep(60*60*7)
+    link = sys.argv[1]
+    time_to_class = sys.argv[2]
+
+    time.sleep(60*60*time_to_class)
     browser_path = "C:\chromedriver"
     chrome_options = webdriver.ChromeOptions()        
     chrome_options.add_argument(browser_path)
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get("https://meet.google.com/mcr-cscp-iuo")
+    driver.get(link)
     mute = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[1]/div/div/div/div[1]')
     camera = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[4]/div[2]')
     join = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[2]/div[1]/div[2]/div/div[1]/div[1]/span/span')
